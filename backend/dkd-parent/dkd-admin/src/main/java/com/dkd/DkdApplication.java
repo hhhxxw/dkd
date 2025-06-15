25da98c599eb07137fffda4ffaaf87dc56b03e01
一只咸鱼的大厂梦 <11516488+hxw6666666@user.noreply.gitee.com>
2025-06-15 21:34:24 +0800

    点位表、合作商表、区域表对应的后端代码，以及manage模块的sql文件

--- backend/dkd-parent/dkd-admin/src/main/java/com/dkd/DkdApplication.java
+++ backend/dkd-parent/dkd-admin/src/main/java/com/dkd/DkdApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * 启动程序
  *
@@ -17,5 +21,16 @@ public class DkdApplication
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(DkdApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  帝可得启动成功   ლ(´ڡ`ლ)ﾞ");
+    }
+    public static void main1(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        boolean[] flag = new boolean[n];
+        dfs(0, n, flag, new ArrayList<>());
+    }
+    public static void dfs(int index, int n, boolean[] flag, List<Integer> current) {
+        if(index == 2) {
+
+        }
     }
 }
